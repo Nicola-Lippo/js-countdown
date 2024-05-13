2b810b56b904788e9de4d89126bd5c96c521db65
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-05-13 14:37:16 +0200

    consegna

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Simulare un count down di 10 secondi che alla fine dice "Buon anno!"
-Potete stampare il messaggio in pagina con un semplice alert.
+stampo il messaggio in pagina con un alert.
--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 'use strict'
 console.log('ci sono')
+//Simulare un count down di 10 secondi che alla fine dice "Buon anno!
+setTimeout(funzioneTempo, 10000);
+
+function funzioneTempo() {
+    //stampo il messaggio in pagina con un alert.
+    alert('Buon anno')
+}
